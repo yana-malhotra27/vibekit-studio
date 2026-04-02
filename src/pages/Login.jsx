30a1c4e41209e54ada54df879b7ead93e6fe6e07
yana-malhotra27 <yana27.ym@gmail.com>
2026-04-02 22:34:15 +0530

    see

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -10,7 +10,7 @@ export default function Login() {
   const handleLogin = async () => {
   try {
     const res = await axios.post(
-      ".netlify/functions/auth-login",
+      "/.netlify/functions/auth-login",
       {
         email,
         password,
